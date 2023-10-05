eec50a198204c029f6d53639864c632189e87c58
Harpreet Singh <harpreet.singh@digimantra.com>
2023-10-05 21:58:58 +0530

    Tab bar setup and Git initialization

---
+++ Avengers/API Service/APIService.swift
@@ -0,0 +1,8 @@
+//
+//  APIService.swift
+//  Avengers
+//
+//  Created by Harpreet Singh on 05/10/23.
+//
+
+import Foundation
---
+++ Avengers/ViewControllers/Characters/CharactersViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CharactersViewController.swift
+//  Avengers
+//
+//  Created by Harpreet Singh on 05/10/23.
+//
+
+import UIKit
+
+class CharactersViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
